4d1cfbc009da20c122dd05ec6e449fa35d26b766
Nat Hill <nat.hill@gusto.com>
2024-06-20 15:31:19 -0500

    nvchad initial commit

---
+++ lua/chadrc.lua
@@ -0,0 +1,19 @@
+-- This file  needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+	theme = "gruvchad",
+  statusline = {
+    theme="minimal"
+  }
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+return M
