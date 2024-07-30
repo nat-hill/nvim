cb632ed5d1c3e2fd2285327c0aeabfa54ca3006c
Nat Hill <nat.hill@gusto.com>
2024-07-30 11:13:21 -0400

    updated nvchad conf

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,34 @@
 require "nvchad.options"
-
+require "setup.harpoon"
 -- add yours here!
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+require('ts_context_commentstring').setup { enable_autocmd = false, }
+require("Comment").setup { pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),}
+
+
+require('telescope').setup{
+  defaults = require('telescope.themes').get_dropdown {
+     -- theme = "dropdown"
+  }
+}
+
+require("nvim-tree").setup({
+  view = { adaptive_size = true },
+})
+  -- pickers = {
+  --   find_files = {
+  --     theme = "dropdown",
+  --   },
+  --   oldfiles = {
+  --     theme = "dropdown",
+  --   },
+  --   live_grep = {
+  --     theme = "dropdown",
+  --   },
+  --   file_paths = {
+  --     theme = "dropdown",
+  --   }
+  -- }
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,7 +15,6 @@ return {
       require "configs.lspconfig"
     end,
   },
-
   {
   	"williamboman/mason.nvim",
   	opts = {
@@ -27,23 +26,67 @@ return {
   	},
   },
   {
-    "numToStr/Comment.nvim",
-    dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
-    config = function()
-    require("Comment").setup {
-        pre_hook =
-        require("ts_context_commentstring.integrations.comment_nim").create_pre_hook(),
-      }
-    end,
+    "JoosepAlviste/nvim-ts-context-commentstring",
   },
-  --
+  {
+    "numToStr/Comment.nvim",
+  },
+  {
+    "ahmedkhalf/project.nvim",
+  },
   -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
+  --   "ThePrimeagen/harpoon",
+  --   branch = "harpoon2",
+  --   dependencies = { "nvim-lua/plenary.nvim" }
   -- },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   -- dependencies = {
+  --   --   "JoosepAlviste/nvim-ts-context-commentstring",
+  --   -- },
+  --   opts = {
+  --     context_commentstring = {
+  --       enable = true,
+  --       enable_autocmd = false,
+  --     }
+  --   }
+  -- },
+  -- 
+	-- {
+	--    'terrortylor/nvim-comment',
+	--    config = function()
+	--    require("nvim_comment").setup({ create_mappings = false,
+	--      hook = function()
+	--      require('ts_context_commentstring').update_commentstring()
+	--      end,
+	--    })
+	--    end,
+	-- },
+	 -- {
+	 -- 	"nvim-treesitter/nvim-treesitter",
+	 --   config = function()
+	 --   require("nvim-treesitter.configs").setup({
+	 --  context_commentstring = {
+	 --    config = {
+	 --    javascript = {
+	 --   __default = '// %s',
+	 --   jsx_element = '{/* %s */}',
+	 --   jsx_fragment = '{/* %s */}',
+	 --   jsx_attribute = '// %s',
+	 --   comment = '// %s',
+	 --    },
+	 --    typescript = {
+	 --    __default = '// %s',
+	 -- 	tsx_element = '{/* %s */}',
+	 -- 	tsx_fragment = '{/* %s */}',
+	 -- 	tsx_attribute = '// %s',
+	 -- 	jsx_element = '{/* %s */}',
+	 -- 	jsx_fragment = '{/* %s */}',
+	 -- 	jsx_attribute = '// %s',
+	 --    __multiline = '/* %s */' 
+	 --     },
+	 --   },
+	 -- 	}})
+	 --  end,
+	 -- },
 }
