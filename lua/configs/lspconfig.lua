b639aaa79e806f660367f575226f72a7d3ec61b0
Nat Hill <nat.hill@gusto.com>
2024-08-09 16:13:44 -0400

    neovim lol

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -25,6 +25,8 @@ lspconfig.tsserver.setup {
 -- sorbet (ruby)
 require('lspconfig').sorbet.setup {
   on_attach = on_attach,
+  on_init = on_init,
+  -- cmd = {"bin/srb", "tc", "--lsp", "--cache-dir", "sorbet"},
+  cmd = {"bin/srb", "tc", "--lsp" },
   capabilities = capabilities,
-  cmd = {"bin/srb", "tc", "--lsp", "--cache-dir", "sorbet"},
 }
