e9cbde29c5901132f36a404b049e82afbb6a4347
nat hill <wnhill13@gmail.com>
2025-01-02 15:35:42 -0700

    added cpp lsp, reconfigured chadrc for new version

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "eslint"}
+local servers = { "html", "cssls", "eslint", "gopls", "clangd"}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -16,7 +16,7 @@ for _, lsp in ipairs(servers) do
 end
 
 -- typescript
-lspconfig.tsserver.setup {
+lspconfig.ts_ls.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
